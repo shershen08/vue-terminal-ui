2611b17fc79aee978debc039818771e17a623352
Mikhail Kuznetcov <michail.kuznetsov@gmail.com>
2018-05-29 15:40:00 +0200

    move CSS to component, some codestyle fixes

---
+++ src/components/vue-term/VueTerminal.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="vue-terminal-wrapper">
+      <div id="terminal" class="boring" data-theme="boring"></div>
+  </div>
+</template>
+
+<script>
+import './ptty.jquery.js'
+import $ from 'jquery'
+
+export default {
+  name: 'VueTerminal',
+  props: {
+    height: {
+      type: String
+    },
+    intro: {
+      type: String,
+      default: ''
+    },
+    consoleSign: {
+      type: String,
+      default: '>>'
+    }
+  },
+  mounted(){
+        var $ptty = $('#terminal').Ptty({
+          i18n: {
+            welcome: this.intro,
+          },
+          ps: this.consoleSign
+        });
+
+        // example - register a command
+        $ptty.register('command', {
+            name : 'hello',
+            method : function(cmd){
+                cmd.out = '<h1>Hello World!</h1>';
+                return cmd;
+            },
+            help : 'A demo command.'
+        });
+  }
+}
+</script>
+
+<style lang="css">
+#terminal {    max-height: 250px;}
+.boring, .boring .prompt, .boring .content{ font-family: "Courier New", Courier, monospace; background-color: #111; color: #ddd; }
+.boring .content{ padding: 15px 15px 0 15px; }
+.boring .prompt{ padding: 0 15px 15px 15px; }
+.boring .loading span::after{content: "⚙"; color: #ddd; font-size: 10em; border-radius: 10em; opacity: 0.4;}
+.boring .content ul{ margin: 0; }
+.boring .prompt .input.show-caret{ color: #ddd; opacity: .85; }
+.boring .prompt .input, .boring .prompt .input::before, .boring .prompt .input::after{ color: transparent; text-shadow:0 0 0 #ddd; }
+.boring .content div .cmd_in .cmd_ps, .boring .prompt .input::before{ padding-right: 10px; }
+.boring .content ul li{ list-style-type: none; }
+.boring div.prompt div.input::after{ font-size: 2em; }
+.boring div.prompt div.input, .boring div.content div div.cmd_in, .boring div.prompt div.input::before{ line-height: 2em; }
+
+#terminal{ position: relative; display: block; overflow-X: hidden; height: 100%;text-align: left; }
+ #terminal div.content div p{ margin: 0; }
+ #terminal div.content div{ clear: both;  word-wrap:break-word; }
+ #terminal div.content div ul{ padding: 0; white-space: normal }
+ #terminal div.content div ul li{ list-style: none; }
+ #terminal div.content div ul.sq-li li{ display: inline-block; text-align: center; padding: 10px; min-width: 5%; }
+ #terminal div.prompt div.input{ width: 100%; white-space:pre-wrap; word-wrap:break-word; cursor: default; outline: none;}
+ #terminal div.prompt div.input::before{ vertical-align: middle; content: attr(data-ps); }
+ #terminal div.prompt div.input::after{ visibility : visible; vertical-align: middle; content: attr(data-caret); }
+ #terminal div.prompt div.input.blink::after{ visibility : hidden; }
+ #terminal div.prompt .hide{ position:absolute; top: -9999em; }
+ #terminal div.loading{ display: none; }
+ #terminal div.loading.working{ display: block; display:flex; justify-content: center; align-items: center;position: fixed;  width: inherit; height: inherit; }
+ #terminal div.loading span{ -webkit-animation: spin 4s linear infinite; -moz-animation: spin 4s linear infinite; -ms-animation: spin 4s linear infinite; -o-animation: spin 4s linear infinite; animation: spin 4s linear infinite; }@-moz-keyframes spin { 100% { -moz-transform: rotate(360deg); } }@-webkit-keyframes spin { 100% { -webkit-transform: rotate(360deg); } }@-ms-keyframes spin { 100% { -ms-transform: rotate(360deg); } }@-o-keyframes spin { 100% { -o-transform: rotate(360deg); } }@keyframes spin { 100% { transform: rotate(360deg); } }
+
+</style>
